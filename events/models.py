ba333cd9f2c96aca8259b86954d0cc0c31b3a6c8
Wilmerso da Silva <wilmersondasilva@gmail.com>
2015-12-25 18:07:33 -0300

    Fix settings

--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Evento(models.Model):
 	nome = models.CharField(max_length=50)
-	descricao = models.TextField()
+	descricao = models.TextField("Descrição")
 	foto = models.CharField(max_length=200)
 	local = models.CharField(max_length=40)
 	data = models.CharField(max_length=20)
