aaea9fdc12cf615823324472f4839ae1b4ec7b31
Wilmerso da Silva <wilmersondasilva@gmail.com>
2015-12-25 18:37:50 -0300

    Admin updated with name

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from . import models
 
+admin.sites.AdminSite.site_header = 'Administração do Events'
+
 # Register your models here.
 admin.site.register(models.Evento)
